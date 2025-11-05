98dfd7b8d05b89fee03de2922a58b10239210dab
Lakshmi Narayana <glnr8919@gmail.com>
2025-11-05 12:33:37 +0530

    tl commit

--- src/test/java/reports/Extentreport.java
+++ src/test/java/reports/Extentreport.java
@@ -20,7 +20,7 @@ public class Extentreport implements ITestListener {
 	 public ExtentTest test;
 	 
 	 	public void onStart(ITestContext context) {
-			sparkreporter = new ExtentSparkReporter(System.getProperty("user.dir")+"\\ReportsSL\\"+"Reportl1.html");
+			sparkreporter = new ExtentSparkReporter(System.getProperty("user.dir")+"\\ReportsSL\\"+"Reportl3.html");
 			sparkreporter.config().setDocumentTitle(" Testing Practise ");
 			sparkreporter.config().setReportName(" Testing--1");
 	     	sparkreporter.config().setTheme(Theme.STANDARD);
